f08e0d9f499e0a89761d8325d076c88ca3f4cb38
titangene <titangene.tw@gmail.com>
2021-03-09 13:43:42 +0800

    feat: shared & main scss styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import './assets/scss/main.scss';
+
 createApp(App)
   .use(router)
   .mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,14 @@ module.exports = {
       entry: 'src/main.js',
       title: '一起話畫 (Paint out loud)'
     }
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import "@/assets/scss/shared.scss";
+        `
+      }
+    }
   }
 };
