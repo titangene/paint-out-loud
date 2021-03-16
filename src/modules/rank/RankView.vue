f338644cc953a9817c8a9342b6607cb5853583c0
titangene <titangene.tw@gmail.com>
2021-03-16 13:04:15 +0800

    feat: rank list use primevue table component instead

--- src/modules/rank/RankView.vue
+++ src/modules/rank/RankView.vue
@@ -1,36 +1,52 @@
 <template>
   <div class="rank">
     <div class="container">
-      <h1 class="rank-title">排行榜</h1>
-      <div class="rank-status">
+      <h1 class="rank-title p-mx-auto p-my-0">排行榜</h1>
+      <div class="rank-status p-mx-auto p-my-3">
         <span>目前排行：{{ current_rank.rank }}</span>
-        <span>分數：{{ current_rank.score }}</span>
+        <span class="p-ml-4">分數：{{ current_rank.score }}</span>
       </div>
 
-      <div class="rank-list-wrap">
-        <div class="rank-list">
-          <div class="rank-item" v-for="item in ranks" :key="item">
-            <div class="ranking-number">
-              <div v-if="isTopThree(item.rank)" class="ranking-number-image">
-                <img
-                  :src="`img/ranked-${item.rank}.png`"
-                  :alt="`ranked ${item.rank}`"
-                />
-              </div>
-              <span v-else class="ranking-number-content">
-                {{ item.rank }}
-              </span>
-            </div>
-            <div class="user">
-              <div class="user-avatar">
-                <img :src="item.avatar_url" alt="avatar" />
-              </div>
-              <span class="user-name">{{ item.user_name }}</span>
-            </div>
-            <div class="score">分數：{{ item.score }}</div>
-          </div>
-        </div>
-      </div>
+      <DataTable
+        :value="ranks"
+        :rowClass="highlightCurrentUser"
+        class="rank-table"
+        responsiveLayout="scroll"
+      >
+        <Column
+          field="rank"
+          header="排行"
+          class="p-text-center"
+          headerClass="table-fixed-width"
+          bodyClass="rank-table-number"
+        >
+          <template #body="{ data }">
+            <img
+              v-if="isTopThree(data.rank)"
+              :src="`img/ranked-${data.rank}.png`"
+              :alt="`ranked ${data.rank}`"
+            />
+            <span v-else>{{ data.rank }}</span>
+          </template>
+        </Column>
+        <Column
+          header="玩家"
+          headerClass="p-text-center"
+          bodyClass="rank-table-user"
+        >
+          <template #body="{ data }">
+            <img :src="data.avatar_url" class="p-mr-2" alt="avatar" />
+            <span class="p-text-truncate">{{ data.user_name }}</span>
+          </template>
+        </Column>
+        <Column
+          field="score"
+          header="分數"
+          class="p-text-center"
+          headerClass="table-fixed-width"
+          bodyClass="rank-table-score"
+        />
+      </DataTable>
     </div>
   </div>
 </template>
@@ -126,10 +142,15 @@ export default {
       return rank => rank <= 3;
     });
 
+    function highlightCurrentUser({ rank }) {
+      return rank === state.current_rank.rank ? 'current-user' : null;
+    }
+
     return {
       ranks: state.ranks,
       current_rank: state.current_rank,
-      isTopThree
+      isTopThree,
+      highlightCurrentUser
     };
   }
 };
@@ -137,23 +158,24 @@ export default {
 
 <style lang="scss" scoped>
 .rank {
-  padding: 40px 0;
-
-  background-color: #a2ddcb;
+  background-color: $robin;
 }
 
 .container {
+  min-height: 100vh;
   max-width: 800px;
   margin: 0 auto;
+  padding: 40px 0;
 }
 
 .rank-title {
   max-width: 400px;
+  width: 90%;
   height: 60px;
+  padding: 0 10px;
   line-height: 60px;
-  margin: 0 auto;
 
-  background-color: #e6b566;
+  background-color: $peach;
   border-radius: 20px;
 
   font-size: 40px;
@@ -162,113 +184,82 @@ export default {
 
 .rank-status {
   max-width: 500px;
+  width: 90%;
   height: 60px;
-  margin: 20px auto;
+  padding: 0 10px;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
-  background-color: #7aa297;
+  background-color: rgba($dark, 0.3);
   color: #fff;
   border-radius: 20px;
 
-  font-size: 24px;
-
-  span + span {
-    margin-left: 20px;
-  }
-}
-
-.rank-list-wrap {
-  height: calc(100vh - 240px);
-
-  overflow: auto;
-}
-
-.rank-list {
-  background-color: #fff;
-}
-
-.rank-item {
-  padding: 6px 10px;
-  display: flex;
-  align-items: center;
-
-  border: 1px solid;
+  font-size: 20px;
 
   @include media(md) {
-    padding: 12px 20px;
+    font-size: 24px;
+  }
+}
+
+::v-deep .rank-table {
+  font-size: 20px;
+
+  @include media(md) {
+    font-size: 24px;
   }
 
-  .ranking-number {
-    flex: 0 0 60px;
+  .current-user {
+    background-color: $butter;
+  }
 
-    font-size: 0;
-    text-align: right;
+  &-number {
+    img {
+      width: 50px;
 
-    &-image {
-      width: 60px;
-
-      img {
-        max-width: 100%;
+      @include media(md) {
+        width: 60px;
       }
     }
 
-    &-content {
-      padding-right: 10px;
+    span {
+      font-size: 24px;
 
-      font-size: 30px;
-      font-weight: 600;
+      @include media(md) {
+        font-size: 28px;
+      }
     }
   }
 
-  .user {
-    padding-left: 20px;
-    flex: 1 0 0;
+  &-user {
+    img {
+      width: 50px;
 
-    &-avatar {
-      display: inline-block;
-      width: 60px;
-      margin-right: 10px;
+      border-radius: 100%;
 
-      font-size: 0;
-      vertical-align: middle;
-
-      img {
-        max-width: 100%;
-
-        border-radius: 100%;
+      @include media(md) {
+        width: 60px;
       }
     }
 
-    &-name {
+    span {
       width: calc(100% - 70px);
       display: inline-block;
 
-      font-size: 24px;
       vertical-align: middle;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-
-      @include media(md) {
-        font-size: 30px;
-      }
     }
   }
+}
 
-  .score {
-    flex: 0 0 110px;
+::v-deep .table-fixed-width {
+  width: 80px;
+}
 
-    font-size: 20px;
-    text-align: right;
-
-    @include media(md) {
-      flex: 0 0 140px;
-
-      font-size: 24px;
-    }
+::v-deep .p-datatable {
+  .p-datatable-thead > tr > th,
+  .p-datatable-tbody > tr > td {
+    padding: 6px;
   }
 }
 </style>
--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -6,8 +6,14 @@ import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 
+// components
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
 export default {
   install(app) {
     app.use(PrimeVue);
+    app.component('DataTable', DataTable);
+    app.component('Column', Column);
   }
 };
