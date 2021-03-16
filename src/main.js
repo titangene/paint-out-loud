9888ee3ee06b35842d181d654445988cf42d2116
titangene <titangene.tw@gmail.com>
2021-03-16 12:37:52 +0800

    feat: use primevue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import PrimeVue from './plugins/primevue';
 
 import './assets/scss/main.scss';
 
 createApp(App)
   .use(router)
+  .use(PrimeVue)
   .mount('#app');
