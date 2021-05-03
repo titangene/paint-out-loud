786cfe5e2517c6e2b7ec68d4fd21f5b97ee1cd95
titangene <titangene.tw@gmail.com>
2021-05-03 17:35:19 +0800

    feat: import PrimeVue TabView & TabPanel component

--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -10,6 +10,8 @@ import 'primeicons/primeicons.css';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Dialog from 'primevue/dialog';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 
@@ -19,6 +21,8 @@ export default {
     app.component('DataTable', DataTable);
     app.component('Column', Column);
     app.component('Dialog', Dialog);
+    app.component('TabView', TabView);
+    app.component('TabPanel', TabPanel);
     app.component('InputText', InputText);
     app.component('Button', Button);
   }
