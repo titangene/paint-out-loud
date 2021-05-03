4c75394529c70fd35a722afa52f42bec00b8c44d
titangene <titangene.tw@gmail.com>
2021-05-03 17:07:22 +0800

    feat: add BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton'
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  display: inline-block;
+  padding: 0.375rem 0.75rem;
+
+  line-height: 1.5;
+  text-align: center;
+  vertical-align: middle;
+
+  border: none;
+  border-radius: 6px;
+}
+</style>
